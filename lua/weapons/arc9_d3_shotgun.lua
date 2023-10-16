4260e44d4ca7346a333dfedfed61f3d545949761
Jayrazer <79886161+Jayrazer@users.noreply.github.com>
2023-10-16 16:45:19 -0400

    Initial commit

---
+++ lua/weapons/arc9_d3_shotgun.lua
@@ -0,0 +1,318 @@
+AddCSLuaFile()
+
+SWEP.Base = "arc9_base"
+
+SWEP.Spawnable = true
+SWEP.Category = "ARC9 - IdTech 4"
+SWEP.SubCategory = "DOOM 3"
+
+SWEP.PrintName = "UAC Combat Shotgun"
+
+SWEP.Class = "Shotgun"
+
+SWEP.Trivia = {
+    Manufacturer = "UAC Small Arms Division",
+    Calibre = "12 Gauge",
+    Mechanism = "Pump Action",
+    Year = "2104"
+}
+
+SWEP.Credits = {
+    Author = "speedonerd",
+    Assets = "Id Software, Upset",
+}
+
+SWEP.Description = [[Standard-issue combat shotgun for UAC Marines. While pump-action shotguns may be archaic, they're still powerful and reliable which has allowed them to remain popular for over 200 years.]]
+
+SWEP.ViewModel = "models/weapons/doom3/c_shotgun.mdl"
+SWEP.WorldModel = ""
+
+SWEP.Slot = 2
+
+SWEP.MirrorVMWM = true
+SWEP.WorldModelOffset = {
+    Pos = Vector(-8, 4, -6),
+    TPIKPos = Vector(-15, 3, 3),
+    Ang = Angle(-10, 0, 180),
+    Scale = 0.75
+}
+SWEP.NoTPIKVMPos = true
+
+SWEP.Crosshair = true
+
+SWEP.DefaultBodygroups = "0000000000000000000000"
+
+-------------------------- DAMAGE PROFILE
+
+SWEP.DamageMax = 130 -- Damage done at point blank range
+SWEP.DamageMin = 100 -- Damage done at maximum range
+
+SWEP.Num = 10
+
+SWEP.DistributeDamage = true
+
+SWEP.DamageRand = 0.1 -- Damage varies randomly per shot by this fraction. 0.1 = +- 10% damage per shot.
+
+SWEP.RangeMin = 1700 -- How far bullets retain their maximum damage for.
+SWEP.RangeMax = 4200 -- In Hammer units, how far bullets can travel before dealing DamageMin.
+
+SWEP.Penetration = 1 -- Units of wood that can be penetrated by this gun.
+SWEP.RicochetChance = 0.1
+
+SWEP.BodyDamageMults = {
+    [HITGROUP_HEAD] = 1.15,
+    [HITGROUP_CHEST] = 1.1,
+    [HITGROUP_STOMACH] = 1,
+    [HITGROUP_LEFTARM] = 1,
+    [HITGROUP_RIGHTARM] = 1,
+    [HITGROUP_LEFTLEG] = 0.9,
+    [HITGROUP_RIGHTLEG] = 0.9,
+}
+
+-------------------------- PHYS BULLET BALLISTICS
+
+SWEP.PhysBulletMuzzleVelocity = 1410.76 * 12
+SWEP.PhysBulletGravity = 2
+SWEP.PhysBulletDrag = 3
+
+-------------------------- MAGAZINE
+
+SWEP.Ammo = "buckshot" -- What ammo type this gun uses.
+
+SWEP.ChamberSize = 0 -- The amount of rounds this gun can chamber.
+SWEP.ClipSize = 8 -- Self-explanatory.
+SWEP.SupplyLimit = 4 -- Amount of magazines of ammo this gun can take from an ARC9 supply crate.
+
+SWEP.ReloadInSights = false -- This weapon can aim down sights while reloading.
+
+-------------------------- FIREMODES
+
+SWEP.RPM = 70
+
+SWEP.Firemodes = {
+    {
+        Mode = 1,
+		PrintName = "Pump Action"
+    }
+}
+
+SWEP.ManualAction = false
+SWEP.ShotgunReload = true
+
+SWEP.BarrelLength = 24
+
+-------------------------- RECOIL
+
+-- General recoil multiplier
+SWEP.Recoil = 1
+
+-- These multipliers affect the predictible recoil by making the pattern taller, shorter, wider, or thinner.
+SWEP.RecoilUp = 1 -- Multiplier for vertical recoil
+SWEP.RecoilSide = 1 -- Multiplier for vertical recoil
+
+-- These values determine how much extra movement is applied to the recoil entirely randomly, like in a circle.
+-- This type of recoil CANNOT be predicted.
+SWEP.RecoilRandomUp = 0.1
+SWEP.RecoilRandomSide = 0.1
+
+SWEP.RecoilDissipationRate = 50 -- How much recoil dissipates per second.
+SWEP.RecoilResetTime = 0.05 -- How long the gun must go before the recoil pattern starts to reset.
+
+SWEP.RecoilAutoControl = 1
+SWEP.RecoilAutoControlHipFire = 2.5
+
+SWEP.RecoilKick = 1
+
+SWEP.UseVisualRecoil = false
+
+SWEP.PhysicalVisualRecoil = false
+
+SWEP.VisualRecoilUp = 8 -- Vertical tilt for visual recoil.F
+SWEP.VisualRecoilSide = 0.8 -- Horizontal tilt for visual recoil.
+SWEP.VisualRecoilRoll = 30 -- Roll tilt for visual recoil.
+SWEP.VisualRecoilPunch = 0.5
+
+SWEP.VisualRecoil = 1
+SWEP.VisualRecoilMultSights = 1
+SWEP.VisualRecoilPositionBump = 1.5
+
+SWEP.VisualRecoilDampingConst = 40
+SWEP.VisualRecoilSpringMagnitude = 1
+
+SWEP.VisualRecoilCenter = Vector(4.525, 4, -3)
+
+-------------------------- SPREAD
+
+SWEP.Spread = 0.1
+
+-------------------------- HANDLING
+
+SWEP.FreeAimRadius = 0 -- In degrees, how much this gun can free aim in hip fire.
+
+SWEP.Sway = 0 -- How much the gun sways.
+
+SWEP.SwayMultMidAir = 4
+SWEP.SwayMultMove = 1.75
+SWEP.SwayMultCrouch = 0.25
+SWEP.SwayMultShooting = 1.5
+
+SWEP.FreeAimRadiusMultSights = 0
+
+SWEP.SwayMultSights = 0.3
+
+SWEP.AimDownSightsTime = 0.35 -- How long it takes to go from hip fire to aiming down sights.
+SWEP.SprintToFireTime = 0.3 -- How long it takes to go from sprinting to being able to fire.
+
+SWEP.SpeedMult = 0.95
+SWEP.SpeedMultSights = 0.75
+SWEP.SpeedMultShooting = 1
+SWEP.SpeedMultCrouch = 1
+
+-------------------------- TRACERS
+
+SWEP.TracerNum = 1 -- Tracer every X
+SWEP.TracerColor = Color(255, 225, 200) -- Color of tracers. Only works if tracer effect supports it. For physical bullets, this is compressed down to 9-bit color.
+
+-------------------------- POSITIONS
+
+SWEP.IronSights = nil
+
+SWEP.SprintAng = Angle(30, -15, 0)
+SWEP.SprintPos = Vector(5, -4, 0)
+
+SWEP.ViewModelFOVBase = 70
+SWEP.ActivePos = Vector(1, 4, -1)
+SWEP.ActiveAng = Angle(5, 6, 0)
+
+SWEP.CrouchPos = Vector(-1, -3, -3)
+SWEP.CrouchAng = Angle(0, 0, 0)
+
+SWEP.CustomizeAng = Angle(90, 0, 0)
+SWEP.CustomizePos = Vector(22, 42, 5)
+SWEP.CustomizeSnapshotFOV = 100
+
+SWEP.BipodPos = Vector(0, 0, 0)
+
+-------------------------- HoldTypes
+
+SWEP.HoldType = "shotgun"
+SWEP.HoldTypeSprint = "normal"
+SWEP.HoldTypeHolstered = "normal"
+SWEP.HoldTypeSights = "rpg"
+SWEP.HoldTypeCustomize = "slam"
+SWEP.HoldTypeBlindfire = "pistol"
+
+SWEP.AnimShoot = ACT_HL2MP_GESTURE_RANGE_ATTACK_AR2
+SWEP.AnimReload = ACT_HL2MP_GESTURE_RELOAD_MAGIC
+SWEP.NonTPIKAnimReload = ACT_HL2MP_GESTURE_RELOAD_AR2
+SWEP.AnimDraw = false
+
+-------------------------- EFFECTS
+
+SWEP.MuzzleParticle = "muzzleflash_shotgun" -- Used for some muzzle effects.
+
+SWEP.ShellCorrectAng = Angle(0, 0, 0)
+SWEP.ShellScale = 1
+SWEP.ShellPhysBox = Vector(0.5, 0.5, 2)
+SWEP.EjectDelay = 0
+SWEP.CaseEffectQCA = 2
+
+SWEP.AfterShotParticle = "barrel_smoke_plume"
+
+SWEP.CamQCA = 1
+SWEP.CamQCA_Mult = 1
+SWEP.CamCoolView = true
+
+-------------------------- SOUNDS
+
+local path = "weapons/arc9_fas/rem870/rem870_"
+local common = "weapons/arc9_fas/"
+
+local insert = {
+    path .. "insert1.wav",
+    path .. "insert2.wav",
+    path .. "insert3.wav",
+}
+
+SWEP.ShootSound = path .. "fire1.wav"
+SWEP.DistantShootSound = "^" .. path .. "distance_fire1.wav"
+SWEP.ShootSoundSilenced = common .. "svd/svd_suppressed_fire1.wav"
+SWEP.DryFireSound = common .. "empty_shotguns.wav"
+
+SWEP.DryFireSingleAction = true
+
+SWEP.TriggerDownSound = common .. "empty/triggerpull_pistol.wav"
+SWEP.TriggerUpSound = common .. "empty/triggerrelease_pistol.wav"
+
+local PumpBack = {
+    path .. "pump_back.wav",
+    path .. "pump_back1.wav",
+    path .. "pump_back2.wav",
+}
+
+local PumpForward = {
+    path .. "pump_forward.wav",
+    path .. "pump_forward1.wav",
+    path .. "pump_forward2.wav",
+}
+
+SWEP.Animations = {
+	["fire"] = {
+		Source = {"fire1", "fire2", "fire3"}
+	},
+	["reload_start"] = {
+		Source = "reloadstart"
+	},
+	["reload_insert"] = {
+		Source = {"reload1", "reload2", "reload3"}
+	},
+	["reload_finish"] = {
+		Source = "reloadend"
+	},
+}
+
+SWEP.SuppressCumulativeShoot = true
+SWEP.SuppressDefaultEvents = true
+
+-------------------------- ATTACHMENTS
+
+SWEP.HideBones = {
+    "shell01",
+    "shell02"
+}
+
+SWEP.ReloadHideBoneTables = {
+    [1] = {
+        "shell01",
+        "shell02"
+    },
+}
+
+SWEP.Hook_PrimaryAttack = function(self)
+    self:DoEject(1, 1)
+end
+
+
+SWEP.AttachmentElements = {
+    ["fas_perk_proficiency"] = {
+        ReloadTimeMult = 1
+    },
+    ["fas_ammo_shotgun"] = {
+        Bodygroups = {
+            {1, 1}
+        }
+    },
+    ["sight"] = {
+        ShotgunReloadIncludesChamber = false
+    }
+}
+
+SWEP.Hook_TranslateAnimation = function(self, anim)
+    if self:HasElement("sight") then
+        return anim .. "_sighted"
+    end
+end
+
+SWEP.Attachments = {
+
+}
